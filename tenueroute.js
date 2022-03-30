b2f2598f1272c642cafc9602ad06a1ec80aa2dfa
DinoGREIL <90773213+DinoGREIL@users.noreply.github.com>
2022-03-30 17:57:22 +0200

    Update and rename tenue.js to tenueroute.js

--- tenueroute.js
+++ tenueroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const tenues = require('../services/tenue');
+const tenues = require('./tenueservices');
 
 /* GET  */
 router.get('/', async function(req, res, next) {
