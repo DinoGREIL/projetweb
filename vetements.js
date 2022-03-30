42e1eaaf64034a0e673cd2b74051b0e557558980
DinoGREIL <90773213+DinoGREIL@users.noreply.github.com>
2022-03-30 17:54:37 +0200

    Update vetements.js

--- vetements.js
+++ vetements.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const vetements = require('../services/vetements');
+const vetements = require('./vetementsservices');
 
 /* GET  */
 router.get('/', async function(req, res, next) {
