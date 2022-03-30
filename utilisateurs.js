e5c921a8f3c65ed0b409108fdeb0dadc94b71dbb
DinoGREIL <90773213+DinoGREIL@users.noreply.github.com>
2022-03-30 17:56:10 +0200

    Update utilisateurs.js

--- utilisateurs.js
+++ utilisateurs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const utilisateurs = require('../services/utilisateurs');
+const utilisateurs = require('./utilisateursservices');
 const bcrypt=require('bcryptjs')
 
 /* GET  */
